435b386e1fe96eb83ebe7c82cd9152fd44f655dd
EC2 Default User <ec2-user@ip-172-31-13-209.ec2.internal>
2022-03-02 12:13:00 +0000

    [Add] 7日分の投稿数表示

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -13,6 +13,10 @@ before_action :authenticate_user!
     @book = Book.new
     @books = Book.all
     @user = current_user
+    @today_book =  @books.created_today
+    @yesterday_book = @books.created_yesterday
+    @this_week_book = @books.created_this_week
+    @last_week_book = @books.created_last_week
   end
 
   def create
